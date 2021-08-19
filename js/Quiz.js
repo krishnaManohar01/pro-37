6b56c797483fc7fb9c2214db5d42b3f5636945a6
krishnaManohar01 <78732694+krishnaManohar01@users.noreply.github.com>
2021-08-19 13:39:31 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,68 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+question.hide()
+    //write code to change the background color here
+background("yellow")
+    //write code to show a heading for showing the result of Quiz
+textsize(30)
+text("Result of quiz",300,50)
+    //call getContestantInfo( ) here
+Contestent.getPlayerInfo()
+
+    //write condition to check if contestantInfor is not undefined
+    if(allContestants!=undefined)
+    {
+      fill("blue")
+      textSize(20)
+      text("Note:Contestant who are answered correctare highlighted in green color",130,230)
+      var y=300
+      for(var plr in allContestants)
+      {
+          var correctAns="2"
+          text(allContestants[plr].name+":"+allContestants[plr].answer,250+50,y)
+          if(correctAns===allContestants[plr].answer)
+
+          fill("green")
+          else
+          fill("red")
+          
+          y=y+50
+      }
+    }
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
